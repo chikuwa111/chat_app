13f3c9aa7cb657666f762ab2387b4c841a17bb37
chikuwa111 <take0shun1111@gmail.com>
2016-08-19 18:11:46 +0900

    検索のユーザークリックでfriendshipがcreateされるようにした

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,18 +1,21 @@
 class FriendshipsController < ApplicationController
-  before_action :logged_in
+  protect_from_forgery :except => [:create, :destroy]
 
   def create
+    user = User.find(params[:to_user_id])
+    inverse_friendship = user.friendships_of_from_user.find_by(to_user_id: current_user.id)
+    if !!(inverse_friendship)
+      redirect_to messages_url
+      return
+    end
+    current_user.friendships_of_from_user.create(to_user_id: user.id)
+    flash[:notice] = "Successfully make friends with #{user.name}!"
+    redirect_to root_url
   end
 
   def destroy
+    Relationships.find(params[:id]).destroy
+    flash[:notice] = "Successfully resolved."
+    redirect_to root_url
   end
-
-  private
-
-    def logged_in
-      if !(user_signed_in?)
-        flash[:alert] = "You need to login."
-        redirect_to new_user_session_url
-      end
-    end
 end
