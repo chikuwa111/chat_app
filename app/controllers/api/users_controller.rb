f5891cda7d061b1d86c485d37084dad88bbf4ca6
chikuwa111 <take0shun1111@gmail.com>
2016-08-20 15:17:13 +0900

    current_userの友達を取得するapiを作成

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,8 +1,14 @@
 module Api
   class UsersController < ApplicationController
+    protect_from_forgery :except => [:index, :friend]
 
     def index
       @users = User.where.not(email: current_user.email)
+      render json: @users
+    end
+
+    def friend
+      @users = current_user.friends
       render json: @users
     end
 
