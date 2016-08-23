0ee9882ff0bf3a83737add091f9ad16580d099fe
chikuwa111 <take0shun1111@gmail.com>
2016-08-23 11:29:38 +0900

    authenticate_userを利用

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in, only: [:index, :find]
+  before_action :authenticate_user!, only: [:index, :find]
 
   def home
   end
@@ -9,13 +9,4 @@ class UsersController < ApplicationController
 
   def find
   end
-
-  private
-
-    def logged_in
-      if !(user_signed_in?)
-        flash[:alert] = "You need to login."
-        redirect_to new_user_session_url
-      end
-    end
 end
