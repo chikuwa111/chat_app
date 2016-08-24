91edbf7fc1ae2ae4e59061a63a1329464a56d143
chikuwa111 <take0shun1111@gmail.com>
2016-08-24 15:52:25 +0900

    不要なコントローラを削除

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,22 +2,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_filter :configure_sign_up_params, only: [:create]
   before_filter :configure_account_update_params, only: [:update]
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
-
-  # POST /resource
-  # def create
-  #   super
-  # end
-
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource
+  # PUT /users
   def update
     file = params[:user][:picture]
     current_user.set_image(file)
@@ -28,20 +13,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
       render :edit
     end
   end
-
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
 
   protected
 
@@ -54,16 +25,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
     def configure_account_update_params
       devise_parameter_sanitizer.for(:account_update) << [:name, :picture]
     end
-
-    # The path used after sign up.
-    # def after_sign_up_path_for(resource)
-    #   super(resource)
-    # end
-
-    # The path used after sign up for inactive accounts.
-    # def after_inactive_sign_up_path_for(resource)
-    #   super(resource)
-    # end
 
     def after_update_path_for(resource)
       user_path(current_user)
