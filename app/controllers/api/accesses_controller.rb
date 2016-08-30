f9750fbb59a1fa902c09a9278ee0d5dd404dc61f
chikuwa111 <take0shun1111@gmail.com>
2016-08-30 15:54:45 +0900

    update_attributesを使うように修正

--- app/controllers/api/accesses_controller.rb
+++ app/controllers/api/accesses_controller.rb
@@ -4,9 +4,9 @@ module Api
     def update
       access = current_user.access_with(params[:to_user_id])
       if access.from_user_id == current_user.id
-        access.update_attribute(:from_user_access, params[:datetime])
+        access.update_attributes(from_user_access: params[:datetime])
       else
-        access.update_attribute(:to_user_access, params[:datetime])
+        access.update_attributes(to_user_access: params[:datetime])
       end
       render text: ''
     end
