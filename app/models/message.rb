d5d4aaf605f7ebf5b8de66e85c75f9bdbc0ee5ff
chikuwa111 <take0shun1111@gmail.com>
2016-08-31 11:43:32 +0900

    message生成でimageあたりの処理をメソッド化

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,6 +6,17 @@ class Message < ActiveRecord::Base
   validates :from_user_id, presence: true
   validates :to_user_id, presence: true
 
+  def applyImage(file)
+    if !file.nil?
+      file_name =  Time.now().to_i.to_s + file.original_filename
+      File.open("public/message_image/#{file_name}", 'wb') {
+        |f| f.write(file.read)
+      }
+      self.image = file_name
+      self.contents = "sent image"
+    end
+  end
+
   def timestamp
     self.created_at.strftime('%Y/%m/%d %H:%M')
   end
